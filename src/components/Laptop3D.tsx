4e7bde5cf6369fbf43e7ce0ab7e267ef06379c62
yeahbois <marcellolienarta663@gmail.com>
2025-11-14 07:14:30 +0700

    make the model responsive

--- src/components/Laptop3D.tsx
+++ src/components/Laptop3D.tsx
@@ -1,13 +1,15 @@
 // Laptop3D.tsx
 import { FC } from 'react';
+import './Laptop3D.css'; // Import your CSS styles
 
 export const Laptop3D: FC = () => {
   return (
-    <iframe 
-      src='https://my.spline.design/genkubgreetingrobot-widw4eAc4aBgnpTMpe3BxyKT/' 
-      frameBorder='0' 
-      width='150%' 
-      height='150%'
-    />
+    <div className="iframe-wrapper">
+      <iframe
+        src="https://my.spline.design/genkubgreetingrobot-widw4eAc4aBgnpTMpe3BxyKT/?embed=1"
+        frameBorder="0"
+        className="responsive-iframe"
+      />
+    </div>
   );
 };
